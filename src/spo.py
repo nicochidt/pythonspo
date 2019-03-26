00adefb1fafcdc79e303fe627b99490cd4303d05
nicochidt <nico.chiaraviglio@gmail.com>
2019-03-26 11:47:53 -0300

    Typo

--- src/spo.py
+++ src/spo.py
@@ -24,7 +24,7 @@ class optimizer:
         S = self.size
         self.x = np.random.uniform( self.lb, self.ub, size = (S,D) )
 
-        fx = inp.apply_along_axis(self.function, 1, self.x)
+        fx = np.apply_along_axis(self.function, 1, self.x)
         i_min = np.argmin(fx)
 
         self.best_x = self.x[i_min,:].copy()

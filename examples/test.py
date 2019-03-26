312a4374b20acce8498a4f7d4ed0bc2e5b3acbbc
nicochidt <nico.chiaraviglio@gmail.com>
2019-03-26 09:59:10 -0300

    Test changed

--- examples/test.py
+++ examples/test.py
@@ -8,11 +8,13 @@ ITERATIONS = 100
 def rastrigin(x):
     # https://en.wikipedia.org/wiki/Rastrigin_function
     # Minimum is  f(0,0,...,0) = 0
+    # search space: 5.12 < x < 5.12
     A = 10
     return  A * x.shape[0] + np.sum( x**2 - A * np.cos(2 * np.pi * x))
 
 def sphere(x):
     # Minimum at f(0,...,0) = 0
+    # search space: -inf < x < inf
     return np.sum(x**2)
 
 def rosenbrock(x):
@@ -26,6 +28,7 @@ def rosenbrock(x):
 def ackley(x):
     # https://en.wikipedia.org/wiki/Ackley_function
     # min is f(0,0) = 0
+    # search space -5 < x < 5
     return -20 * np.exp( -0.2 * np.sqrt( 0.5 * np.sum(x**2))) - np.exp( 0.5 * np.sum(np.cos(2*np.pi*x))) + np.e + 20
 
 def eggholder(x):
@@ -45,19 +48,62 @@ def crossintray(x):
     x = x[0]
     return -0.0001 * (np.abs(np.sin(x) * np.sin(y) * np.exp(np.abs(100 - np.sqrt(x**2 + y **2) / np.pi))) + 1 ) ** 0.1
 
-args = {
+
+functions = {
+        'rastrigin': {
+            'f': rastrigin,
+            'lb': [-5.12, -5.12],
+            'ub': [5.12, 5.12],
+            'min': [0,0],
+            'fmin': 0
+            },
+        'sphere': {
+            'f': sphere,
+            'lb': [-1000, -1000],
+            'ub': [1000, 1000],
+            'min': [0,0],
+            'fmin': 0
+            },
+        'ackley': {
+            'f': ackley,
+            'lb': [ -5, -5],
+            'ub': [ 5, 5],
+            'min': [ 0, 0],
+            'fmin': 0
+            },
+        'eggholder': {
+            'f': eggholder,
+            'lb': [-512, -512],
+            'ub': [512, 512],
+            'min': [512, 404.2319],
+            'fmin': -959.6407
+            },
+        'crossintray': {
+            'f': crossintray,
+            'lb': [-10, -10],
+            'ub': [10,10],
+            'min': [(1.34941,1.34941), (-1.34941,1.34941), (1.34941,-1.34941), (-1.34941,-1.34941)],
+            'fmin': [ -2.06261, -2.06261, -2.06261, -2.06261]
+            }
+        }
+
+for k,v in functions.iteritems():
+
+    args = {
         'swarm_size': SWARM_SIZE,
         'max_iterations': ITERATIONS,
-        'function': ackley,
-        'lb': [ -10,-10],  # lower bond for search space
-        'ub': [10,10],     # upper bond for search space
+        'function': v['f'],
+        'lb': v['lb'],   # lower bond for search space
+        'ub': v['ub'],     # upper bond for search space
         'omega': 0.5,
         'phi_r': 0.5,
         'phi_g': 0.5,
         'debug': False   # if True best position and value of each iteration is printed
      }
-
-opt = spo.optimizer(**args)
-x, fx = opt.run()
-print "[+] Algorithm returned %s with value %f" % ( x, fx)
+    print "[+] Optimizing function %s" % k
+    opt = spo.optimizer(**args)
+    x, fx = opt.run()
+    print "[+] Algorithm returned %s with value %f" % ( x, fx)
+    print "[+] Function minimum is at %s with value %s" % (v['min'], v['fmin'])
+    print "\n"
 

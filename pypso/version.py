8db22f945123eff671877b7c1b2d0a997ef78c76
nicochidt <nico.chiaraviglio@gmail.com>
2019-05-09 11:15:19 +0200

    New estructure

--- pypso/version.py
+++
@@ -1,7 +0,0 @@
-__author__    = 'Nicolas Chiaraviglio'
-__email__     = 'nico.chiaraviglio@gmail.com'
-__copyright__ = 'Copyright 2019, Nicolás Chiaraviglio'
-__credits__   = []
-__license__   = 'GPL'
-__version__   = '1.0.0'
-__status__    = 'Production'
